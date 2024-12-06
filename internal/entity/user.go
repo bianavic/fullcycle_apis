092aaed9dbabd8c7c0c905c09b2f1068d47f0e23
bianavic <bianavic@gmail.com>
2024-12-05 23:16:02 -0300

    06: criando entidade user e jwt

---
+++ internal/entity/user.go
@@ -0,0 +1,32 @@
+package entity
+
+import (
+	"github.com/bianavic/fullcycle_apis/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// entity - regras de negocio
+type User struct {
+	ID       entity.ID `json:"ID"`
+	Name     string    `json:"Name"`
+	Email    string    `json:"Email"`
+	Password string    `json:"Password"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}

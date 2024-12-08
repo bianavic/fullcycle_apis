ce1fed3206f2645d59f2415e305ba67b4d099886
bianavic <bianavic@gmail.com>
2024-12-08 10:23:13 -0300

    15 e 16: finalizando testes de productDB e altera tipo da propriedade price de int para float64

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -28,7 +28,7 @@ func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error
 	var products []entity.Product
 	var err error
 	if sort != "" && sort != "asc" && sort != "desc" {
-		sort = "asc" // forcar que seja asc
+		sort = "asc"
 	}
 	if page != 0 && limit != 0 {
 		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&products).Error

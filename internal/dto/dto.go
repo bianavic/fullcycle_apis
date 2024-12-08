b3a9860ddd2926a6b56d1450581c95a8415b2b25
bianavic <bianavic@gmail.com>
2024-12-08 14:59:05 -0300

    25 e 26: falando sobre JWT e gerando JWT

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -11,3 +11,8 @@ type CreateUserInput struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type GetJWTInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -2,20 +2,26 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/go-chi/jwtauth"
+	"net/http"
+	"time"
+
 	"github.com/bianavic/fullcycle_apis/internal/dto"
 	"github.com/bianavic/fullcycle_apis/internal/entity"
 	"github.com/bianavic/fullcycle_apis/internal/infra/database"
-	"log"
-	"net/http"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB       database.UserInterface
+	Jwt          *jwtauth.JWTAuth
+	JwtExpiresIn int
 }
 
-func NewUserHandler(db database.UserInterface) *UserHandler {
+func NewUserHandler(db database.UserInterface, jwt *jwtauth.JWTAuth, jwtExpiresIn int) *UserHandler {
 	return &UserHandler{
-		UserDB: db,
+		UserDB:       db,
+		Jwt:          jwt,
+		JwtExpiresIn: jwtExpiresIn,
 	}
 }
 
@@ -23,13 +29,11 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	u, err := entity.NewUser(user.Name, user.Email, user.Password)
 	if err != nil {
-		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -39,4 +43,39 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// gerar token formato string
+	// passa dados q envia no payload (token) dentro do encode
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
+	})
+
+	// para serializar os dados no json: criada struct anonima q recebe um access token
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: tokenString,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
